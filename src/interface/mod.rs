b2fc180819d4c5e5b1ac75e3d614eb5c2ccc3be1
Luan Almeida <luan.almeida@jbtec.com.br>
2024-10-23 21:03:31 -0300

    feat: create a button with css

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,22 +1,48 @@
-use gtk4::glib::ExitCode;
-use gtk4::prelude::*;
-use gtk4::{glib, Application, ApplicationWindow};
+use gtk4::gdk::Display;
+use gtk4::{prelude::*, style_context_add_provider_for_display, Button, CssProvider, STYLE_PROVIDER_PRIORITY_APPLICATION};
+use gtk4::{Application, ApplicationWindow};
 
-pub fn main() -> glib::ExitCode {
+
+fn on_button_click(_button: &Button) {
+    println!("clicked");
+}
+
+pub fn on_activate(application: &gtk4::Application) {
+    let provider = CssProvider::new();
+    provider.load_from_path("src/interface/style.css");
+
+    style_context_add_provider_for_display(
+        &Display::default().expect("Error initializing gtk css provider"),
+        &provider,
+        STYLE_PROVIDER_PRIORITY_APPLICATION
+    );
+
+    let window = ApplicationWindow::builder()
+        .application(application)
+        .default_height(400)
+        .default_width(400)
+        .title("Hello, file compressor")
+        .build();
+
+    let button = Button::with_label("Click me");
+    button.connect_clicked(on_button_click);
+    button.add_css_class("btn");
+
+    let container = gtk4::Box::new(gtk4::Orientation::Vertical,0);
+    container.set_valign(gtk4::Align::Center);
+    container.set_halign(gtk4::Align::Center);
+    container.append(&button);
+
+    window.set_child(Some(&container));
+    window.present();
+}
+
+pub fn main() {
     let app = Application::builder()
         .application_id("app.fileCompressor")
         .build();
 
-    app.connect_activate(|app| {
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .default_width(320)
-            .default_height(320)
-            .title("Hello, File compressor")
-            .build();
+    app.connect_activate(on_activate);
 
-        window.present();
-    });
     app.run();
-    ExitCode::SUCCESS
 }
